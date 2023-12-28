0627618dc72cbc7915cee68729a9e8fbb882bc99
shrayanchummu <shrayanchummu@gmail.com>
2023-12-29 00:44:57 +0530

    Adding different type of errors in ERRORHANDLER middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,47 @@
+const { constants } = require("../constants");
+
 const errorHandler=(err,req,res,next)=>{
     const statusCode=res.statusCode?res.statusCode:500;
-    res.send({
-        message:err.message,
-        stackTrace:err.stack
-    });
+    switch(statusCode){
+        case constants.VALIDATION_ERROR:
+            res.send({
+                title:"VALIDATION ERROR",
+                message:err.message,
+                stackTrace:err.stack
+            });
+            break;
+            case constants.UNAUTHORIZED:
+            res.send({
+                title:"UNAUTHORIZED",
+                message:err.message,
+                stackTrace:err.stack
+            });
+            break;
+            case constants.FORBIDDEN:
+            res.send({
+                title:"FORBIDDEN",
+                message:err.message,
+                stackTrace:err.stack
+            });
+            break;
+        case constants.NOT_FOUND:
+            res.send({
+                title:"NOT FOUND",
+                message:err.message,
+                stackTrace:err.stack
+            });
+            break;
+        case constants.SERVER_ERROR:
+            res.send({
+                title:"SERVER ERROR",
+                message:err.message,
+                stackTrace:err.stack
+            });
+            break;
+        default:
+            break;
+    }
+    
 };
 
 module.exports=errorHandler;
