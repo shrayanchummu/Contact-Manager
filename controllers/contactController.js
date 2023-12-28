91e1b661bbd9646aa83ef3d4b2f0799c2b51c20b
shrayanchummu <shrayanchummu@gmail.com>
2023-12-29 00:22:22 +0530

    handling POST request (what if no body params)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -19,6 +19,12 @@ const getContact=(req,res)=>{
 // @access PUBLIC
 
 const createContact=(req,res)=>{
+    console.log('The Request Body is:',req.body);
+    const {name,email,phone}=req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
     res.status(200).send({message:`Create contact`});
 };
 
