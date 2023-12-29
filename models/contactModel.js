3f1a42e39b29e24a406f7eb99c7c4d9f13b46bc3
shrayanchummu <shrayanchummu@gmail.com>
2023-12-29 23:39:34 +0530

    Created Contact MODEL (SCHEMA)

---
+++ models/contactModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const contactSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required=[true,"Please enter Contact Name"]
+    },
+    email:{
+        type:String,
+        required=[true,"Please enter Contact Email"]
+    },
+    phone:{
+        type:String,
+        required=[true,"Please enter Contact Phone Number"]
+    }
+},{
+    timestamps:true
+});
+
+module.exports=mongoose.model("Contact",contactSchema);
