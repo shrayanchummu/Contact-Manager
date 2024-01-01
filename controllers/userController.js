a1f834222d123dcf6a7f3826c822a20cb848eefc
shrayanchummu <shrayanchummu@gmail.com>
2024-01-01 22:44:08 +0530

    Implemented USERMODEL in USERCONTROLLER (register api)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,26 @@
 const asyncHandler=require('express-async-handler');
+const User=require('../models/userModel')
 
 // @desc Registers Users
 // @route POST /api/user/register
 // @access PUBLIC
-const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).send({message:"Register the User"});
+const registerUser=asyncHandler(async(req,res,next)=>{
+    const {username,email,password}=req.body;
+    if(!username||!email||!password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email})
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already Registered");
+    }
+    const newUser= await User.create({
+        username,
+        email,
+        password
+    });
+    res.status(201).send(newUser);
 });
 
 // @desc Login User Info
