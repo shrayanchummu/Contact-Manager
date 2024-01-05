9a4321ed07b34a79d0d77d4b287023f428ab76bf
shrayanchummu <shrayanchummu@gmail.com>
2024-01-06 00:47:28 +0530

    Implemented RATE-LIMITING Feature (in server.js)

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express=require('express');
 // const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
+const rateLimit = require('express-rate-limit');
 const dotenv=require('dotenv').config();
 
 connectDb();
 const app=express();
+
+//Rate Limiting
+const limiter=rateLimit({
+    windowMs:10*60*1000, //10 Minutes
+    max:50 // 50 requests in 10 minutes
+});
+app.use(limiter);
+app.set('trust proxy',1);
+
 
 app.use(express.json());
 app.use('/api/contacts',require('./routes/contactRoutes'));
